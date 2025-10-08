91ec16bb7973bd68af951d1ce4d48521df5e34ba
jozles <pinkasfeld@combox.fr>
2025-10-08 20:31:28 +0200

    mcp4901
    en cours

--- eval_MCP4901/src/mcp4901.cpp
+++ eval_MCP4901/src/mcp4901.cpp
@@ -33,8 +33,12 @@ void setup() {
 
 
   Serial.begin(115200);
-  while (!Serial);  
-  Serial.print("+start MCP4901 ");
+  //while (!Serial);  
+  Serial.print("+start MCP4901 ");delay(1000);
+
+  POWER_ON_MCP
+
+  delay(1000);
 
   CE_INIT
   CSN_INIT
@@ -45,15 +49,15 @@ void setup() {
   //uint16_t value=0x0208;   //0000 0010 0000 1000
 
 
-  uint8_t vbuf=1;
-  uint8_t vga=1;
-  uint8_t vshdn=1;
+  uint8_t vbuf=0;   // 1 buffered ; 0 unbuffered
+  uint8_t vga=1;    // 1 gain 1 ; 0 gain 2
+  uint8_t vshdn=1;  // 1 active ; 0 shutdown
 
   byte value[]={0x00,0x00};
 
   
 
-  uint16_t val=0x07fe;
+  uint16_t val=0x07FF; //0x0db0;      //x0ffe;
 
   value[0]=val>>8;
   value[1]=val&0xff;
--- eval_MCP4901/src/mcp4901.h
+++ eval_MCP4901/src/mcp4901.h
@@ -5,11 +5,26 @@
 
 #define CE_PIN     1          // pin pour CE du nrf
 #define CSN_PIN    2         // pin pour CS du SPI
+#define CLKPIN     5
+#define MISOPIN    4
+#define MOSIPIN    3
+
+#define PORT_PWR_MCP PORTD
+#define DDR_PWR_MCP DDRD
+#define BIT_PWR_MCP 7
+#define POWER_ON_MCP bitSet(DDR_PWR_MCP,BIT_PWR_MCP);bitClear(PORT_PWR_MCP,BIT_PWR_MCP);
+#define POWER_OFF_MCP bitSet(PORT_PWR_MCP,BIT_PWR_MCP);
 
 #define PORT_CSN PORTB
 #define PORT_CE  PORTB
+#define PORT_MOSI PORTB
+#define PORT_MISO  PORTB
+#define PORT_CLK PORTB
 #define DDR_CSN DDRB
 #define DDR_CE  DDRB
+#define DDR_MOSI DDRB
+#define DDR_MISO  DDRB
+#define DDR_CLK DDRB
 
 #define CE_INIT   bitSet(DDR_CE,CE_PIN); //pinMode(CE_PIN,OUTPUT);
 #define CE_OFF    bitClear(DDR_CE,CE_PIN); //pinMode(CE_PIN,INPUT);
@@ -20,12 +35,11 @@
 #define CSN_HIGH  bitSet(PORT_CSN,CSN_PIN); //digitalWrite(CSN_PIN,HIGH);
 #define CSN_LOW   bitClear(PORT_CSN,CSN_PIN); //digitalWrite(CSN_PIN,LOW);
 
-#define CLKPIN    13
-#define MISOPIN   12
-#define MOSIPIN   11
+
 #define SPI_INIT  SPI.beginTransaction(SPISettings(4000000,MSBFIRST,SPI_MODE0));
 #define SPI_START SPI.begin();
-#define SPI_OFF   SPI.end();pinMode(MOSIPIN,INPUT);pinMode(CLKPIN,INPUT);
+#define SPI_OFF   SPI.end();bitClear(DDR_MOSI,MOSI_PIN);bitClear(DDR_CLK,CLK_PIN); //digitalWrite(CSN_PIN,LOW);
+
 
 #define BUF 14
 #define GA  13
